0e62e271c9b7970b22f232097c1c873eb1bb2e03
Nick Bumbarger <nbumbarger@gmail.com>
2016-10-28 14:11:24 -0400

    Adds form structure and style

--- app/assets/scripts/components/upload-form.js
+++ app/assets/scripts/components/upload-form.js
@@ -17,6 +17,29 @@ var UploadForm = React.createClass({
           <div className='fold__body'>
             The CSV parser is able to recognize the following attribute names and characteristics:
           </div>
+
+          <fieldset className='form__fieldset'>
+
+            <div className='form__group'>
+              <label className='form__label' htmlFor='form-input'>API Token</label>
+              <div className='form__input-group'>
+                <input type='text' className='form__control form__control--medium' id='form-input' placeholder='Please enter your uploader API token' />
+              </div>
+            </div>
+
+            <div className='form__group'>
+              <label className='form__label' htmlFor='form-file'>Please select a CSV file</label>
+              <input type='file' className='form__control' id='form-file' />
+            </div>
+
+            <div className='form__group'>
+              <label className='form__label' htmlFor='form-textarea'>Error messages</label>
+              <textarea className='form__control' id='form-textarea' rows='4' placeholder='Error messages will appear here, if applicable'></textarea>
+            </div>
+
+            <button className='button button--base' type='button'><span>Submit CSV</span></button>
+
+          </fieldset>
         </div>
       </section>
     );
