0eb98c2148d37ce2ee0a4b3d3adf91d527cec1ca
Nick Bumbarger <nbumbarger@gmail.com>
2016-11-21 10:53:38 -0500

    Fix fetch function

--- app/assets/scripts/utils/s3-upload.js
+++ app/assets/scripts/utils/s3-upload.js
@@ -2,7 +2,7 @@
 import fetch from 'isomorphic-fetch';
 
 export const getSignedUrl = (filename, token) => {
-  return fetch.fetchUrl(`https://api.openaq-staging.org/v1/upload?filename=${filename}&token=${token}&t=${new Date().getTime()}`)
+  return fetch(`https://api.openaq-staging.org/v1/upload?filename=${filename}&token=${token}&t=${new Date().getTime()}`)
     .then((response) => response.json())
     .then((s3Data) => s3Data);
 };
